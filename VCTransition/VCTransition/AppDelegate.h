f283be84b0e167c224d0d90017896cb78b59c23b
liyy <1290374862@qq.com>
2016-11-18 15:56:13 +0800

    iOS自定义ViewController转场动画

---
+++ VCTransition/VCTransition/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VCTransition
+//
+//  Created by liyy on 16/11/18.
+//  Copyright © 2016年 hihome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
