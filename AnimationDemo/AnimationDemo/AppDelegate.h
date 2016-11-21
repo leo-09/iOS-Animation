1e64651933037d1073ae2feb746c1f35068c32b7
liyy <1290374862@qq.com>
2016-11-21 09:33:08 +0800

    iOS动画的demo

---
+++ AnimationDemo/AnimationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimationDemo
+//
+//  Created by liyy on 16/11/21.
+//  Copyright © 2016年 liyy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
